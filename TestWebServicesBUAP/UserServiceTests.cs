2b04fbed5b8d2eb37aa4d4cb164cc36bd366417c
Sergio A. Martínez Fernández <salberto.mtz@gmail.com>
2021-03-19 21:06:30 -0600

    WIP: UpdateUserInfo and test initial

--- TestWebServicesBUAP/UserServiceTests.cs
+++ TestWebServicesBUAP/UserServiceTests.cs
@@ -7,17 +7,50 @@ namespace TestWebServicesBUAP
     public class UserServiceTests
     {
         private IUserService usrSrv;
-        private string userInfoJSON;
+
+        public enum JSONType
+        {
+            Valid,
+            Invalid,
+            Malformed
+        }
+
         [OneTimeSetUp]
         public void Init()
         {
             usrSrv = new UserService();
-            userInfoJSON = @"{
-                                'correo': 'mi.correo@mail.net',
-                                'nombre': 'Jorge Luis Borges',
-                                'rol': 'ventas',
-                                'telefono': '222-7-18-62-98'
+           
+        }
+        private string getTestJSON(JSONType type)
+        {
+            string userInfoJSON = "";
+
+            switch (type)
+            {
+                case JSONType.Valid:
+                    userInfoJSON = @"{
+                                'correo': 'fulano.lopez@mail.net',
+                                'nombre': 'Fulano López',
+                                'rol': 'rh',
+                                'telefono': '222-7-18-43-56'
                               }";
+                    break;
+                case JSONType.Invalid:
+                    userInfoJSON = @"{
+                                'correo': 'fulano.lopez@mail.net',                              
+                                'rol': 'rh',
+                                'telefono': '222-7-18-43-56'
+                              }";
+                    break;
+                case JSONType.Malformed:
+                    userInfoJSON = @"{
+                                'correo': 'fulano.lopez@mail                               
+                                'rol': 'rh',
+                                'telefono': '222-7-18-43-56'
+                              }";
+                    break;
+            }
+            return userInfoJSON;
         }
 
         [TestCase(500)]
@@ -44,10 +77,26 @@ namespace TestWebServicesBUAP
             return res.Code;
         }
 
-        [TestCase("pruebas1", "12345678b", "pruebas2", ExpectedResult = 402)]
-        public int TestSetUserInfo(string user, string pass, string searchedUser)
+        
+        [TestCase("pruebas1", "12345678a", "pruebas2", JSONType.Valid, ExpectedResult = 504)]
+        [TestCase("pruebas2", "12345678b", "pruebas2", JSONType.Valid, ExpectedResult = 506)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Valid, ExpectedResult = 402)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Invalid, ExpectedResult = 304)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Malformed, ExpectedResult = 305)]
+        public int TestSetUserInfo(string user, string pass, string searchedUser, JSONType type)
         {
-            Respuesta res = usrSrv.SetUserInfo(user, pass, searchedUser, userInfoJSON);
+            Respuesta res = usrSrv.SetUserInfo(user, pass, searchedUser, getTestJSON(type));
+            return res.Code;
+        }
+
+        [TestCase("pruebas1", "12345678a", "pruebas2", JSONType.Valid, ExpectedResult = 504)]
+        [TestCase("pruebas2", "12345678b", "pruebas2asdasd", JSONType.Valid, ExpectedResult = 507)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Valid, ExpectedResult = 403)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Invalid, ExpectedResult = 304)]
+        [TestCase("pruebas2", "12345678b", "pruebas1", JSONType.Malformed, ExpectedResult = 305)]
+        public int TestUpdateUserInfo(string user, string pass, string searchedUser, JSONType type)
+        {
+            Respuesta res = usrSrv.UpdateUserInfo(user, pass, searchedUser, getTestJSON(type));
             return res.Code;
         }
     }
--- WebServicesBUAP/UserService.svc.cs
+++ WebServicesBUAP/UserService.svc.cs
@@ -25,13 +25,26 @@ namespace WebServicesBUAP
         };
         readonly IFirebaseClient client;
 
-        readonly string[] allowedRoles = { "rh" };
+        private readonly string[] allowedRoles = { "rh" };
 
         public UserService()
         {
             client = new FireSharp.FirebaseClient(config);
         }
 
+        /// <summary>
+        /// Authentica y verifica los permisos de un usuario.
+        /// </summary>
+        /// <param name="user">
+        /// El identificador del usuario (nombre de usuario).
+        /// </param>
+        /// <param name="pass">
+        /// La contraseña del usuario.
+        /// </param>
+        /// <returns>
+        /// <seealso cref="WebServicesBUAP.Respuesta"/>
+        /// Un objeto con el código, mensaje y estatus de respuesta.
+        /// </returns>
         public Respuesta Authenticate(string user, string pass)
         {
 
@@ -47,12 +60,22 @@ namespace WebServicesBUAP
         
 
             UserInfo userInfo = GetUserInfo(user);
-            bool cont = allowedRoles.Contains(userInfo.Rol);
-            if (!cont) return GetResponse(504);
+            
+            if (userInfo == null || !allowedRoles.Contains(userInfo.Rol)) return GetResponse(504);
 
             return GetResponse(99);
         }
 
+        /// <summary>
+        /// Obtiene la Respuesta de acuerdo a un código numérico.
+        /// </summary>
+        /// <param name="code">
+        /// El código de la respuesta.
+        /// </param>
+        /// <returns>
+        /// <seealso cref="WebServicesBUAP.Respuesta"/>
+        /// El objeto de respuesta.
+        /// </returns>
         public Respuesta GetResponse(int code)
         {
             FirebaseResponse fireRes = client.Get("respuestas/" + code);
@@ -74,12 +97,44 @@ namespace WebServicesBUAP
             return new Respuesta();
         }
 
+        /// <summary>
+        /// Insertar datos del usuario que son: correo, nombre, rol y teléfono.
+        /// </summary>
+        /// <param name="user">Nombre de usuario de autenticación.</param>
+        /// <param name="pass">La contraseña de autenticación.</param>
+        /// <param name="searchedUser">El identificador de los nuevos datos.</param>
+        /// <param name="userInfoJSON">
+        /// El JSON con la información del usuario.
+        /// Formato:
+        /// <code>
+        /// {
+        ///     "correo": "correo@mail.com",
+        ///     "rol": "userRol",
+        ///     "telefono": "123-2-312-23",
+        ///     "nombre": "Full User Name"
+        /// }
+        /// </code>
+        /// </param>
+        /// <returns>
+        /// <seealso cref="WebServicesBUAP.Respuesta"/>
+        /// La respuesta de la operación.
+        /// </returns>
         public Respuesta SetUserInfo(string user, string pass, string searchedUser, string userInfoJSON)
         {
             Respuesta res = Authenticate(user, pass);
             
-            if (res.Status == "error") return res;                        
-            if (!ValidateJSON(userInfoJSON)) return GetResponse(305);
+            if (res.Status == "error") return res;
+            try
+            {
+                bool isValid = ValidateJSON(userInfoJSON);
+                if (!isValid) return GetResponse(304);
+            }
+            catch (JsonReaderException)
+            {
+
+                return GetResponse(305);
+            }
+           
             if (UserInfoExists(searchedUser)) return GetResponse(506);
 
             UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(userInfoJSON);
@@ -99,17 +154,84 @@ namespace WebServicesBUAP
             return new Respuesta();
         }
 
+        /// <summary>
+        /// Actualizar correo, nombre, rol ó teléfono de una cuenta.
+        /// </summary>
+        /// <param name="user">Nombre de usuario de autenticación.</param>
+        /// <param name="pass">La contraseña de autenticación.</param>
+        /// <param name="searchedUser">El identificador de los nuevos datos.</param>
+        /// <param name="userInfoJSON">
+        /// El JSON con la información del usuario.
+        /// Formato:
+        /// <code>
+        /// {
+        ///     "correo": "correo@mail.com",
+        ///     "rol": "userRol",
+        ///     "telefono": "123-2-312-23",
+        ///     "nombre": "Full User Name"
+        /// }
+        /// </code>
+        /// </param>
+        /// <returns>
+        /// <seealso cref="WebServicesBUAP.Respuesta"/>
+        /// La respuesta de la operación.
+        /// </returns>
         public Respuesta UpdateUserInfo(string user, string pass, string searchedUser, string userInfoJSON)
         {
-            return new Respuesta();
+            Respuesta res = Authenticate(user, pass);
+
+            if (res.Status == "error") return res;
+            try
+            {
+                bool isValid = ValidateJSON(userInfoJSON);
+                if (!isValid) return GetResponse(304);
+            }
+            catch (JsonReaderException)
+            {
+
+                return GetResponse(305);
+            }
+
+            if (!UserInfoExists(searchedUser)) return GetResponse(507);
+
+            UserInfo userInfo = JsonConvert.DeserializeObject<UserInfo>(userInfoJSON);
+
+            FirebaseResponse fireRes = client.Update<UserInfo>("usuarios_info/" + searchedUser, userInfo);
+
+            if (fireRes != null)
+            {
+                res = GetResponse(403);
+            }
+
+            return res;
         }
 
+        /// <summary>
+        /// Verifica si ya existe una entrada de información de usuario en la base de datos.
+        /// </summary>
+        /// <param name="user">
+        /// El identificador del usuario (nombre de usuario)
+        /// </param>
+        /// <returns>
+        /// <seealso cref="System.Boolean"/>
+        /// Verdadero si existe de lo contrario falso.
+        /// </returns>
         private bool UserInfoExists(string user)
         {
             FirebaseResponse fireRes = client.Get("usuarios_info/" + user);
             return !(fireRes == null || fireRes.Body.Trim('"') == "null");
         }
 
+        /// <summary>
+        /// Obtiene la información de un usuario.
+        /// </summary>
+        /// <param name="user">
+        /// El identificador del usuario (nombre de usuario)
+        /// </param>
+        /// <returns>
+        /// <seealso cref="WebServicesBUAP.UserInfo"/>
+        /// La instancia con la información del usuario.
+        /// </returns>
         private UserInfo GetUserInfo(string user)
         {
             FirebaseResponse fireRes = client.Get("usuarios_info/" + user);
@@ -117,6 +239,18 @@ namespace WebServicesBUAP
             return userInfo;
         }
 
+        /// <summary>
+        /// Valida un JSON que representa la información de un usuario.
+        /// </summary>
+        /// <param name="userInfoJSON">
+        /// El JSON que representa la información del usuario.
+        /// </param>
+        /// <returns>
+        /// <seealso cref="System.Boolean"/>
+        /// Verdadero si userInfoJSON es válido o falso si no lo es.
+        /// </returns>
+        /// <exception cref="Newtonsoft.Json.JsonReaderException">
+        /// </exception>
         private bool ValidateJSON(string userInfoJSON)
         {
             string schemaJson = @"{
@@ -124,11 +258,12 @@ namespace WebServicesBUAP
                 'type': 'object',
                 'properties': 
                     {
-                        'correo': { 'type': 'string' },
-                        'nombre': { 'type': 'string' },
-                        'rol': { 'type': 'string' },
-                        'telefono': { 'type': 'string' },
-                    }
+                        'correo': { 'type': 'string', 'minLength': 1 },
+                        'nombre': { 'type': 'string', 'minLength': 1 },
+                        'rol': { 'type': 'string', 'minLength': 1 },
+                        'telefono': { 'type': 'string', 'minLength': 1 },
+                    },
+                'required': ['correo', 'nombre', 'rol', 'telefono']
                 }";
             JSchema schema = JSchema.Parse(schemaJson);
             JObject userInfo = JObject.Parse(userInfoJSON);
